205fee7e51946704d9ac6a9fcbe373e8c1f3f18e
David Salgado Soltero <david.salgados@edu.uah.es>
2026-04-15 09:15:50 +0200

    codigoprofe

--- CajeroAutomatico/src/test/java/com/mycompany/cajeroautomatico/CajeroAutomaticoTest.java
+++ CajeroAutomatico/src/test/java/com/mycompany/cajeroautomatico/CajeroAutomaticoTest.java
@@ -4,6 +4,11 @@
  */
 package com.mycompany.cajeroautomatico;
 
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,49 +17,41 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author grodriguez
  */
 public class CajeroAutomaticoTest {
-    
-    public CajeroAutomaticoTest() {
-    }
 
-    /**
-     * Test of iniciarSesion method, of class CajeroAutomatico.
-     */
-    @Test
-    public void testIniciarSesion() {
-        System.out.println("iniciarSesion");
-        String ccc = "";
-        CajeroAutomatico instance = null;
-        instance.iniciarSesion(ccc);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    private CajeroAutomatico CajeroBajoPrueba;
+    private PasarelaPago mock;
 
-    /**
-     * Test of cerrarSesion method, of class CajeroAutomatico.
-     */
     @Test
-    public void testCerrarSesion() {
-        System.out.println("cerrarSesion");
-        CajeroAutomatico instance = null;
-        instance.cerrarSesion();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testRealizarRetirada1() {
+        System.out.println("Caso 1: pasarela no bloqueada y cuenta con saldo");
 
-    /**
-     * Test of realizarRetirada method, of class CajeroAutomatico.
-     */
-    @Test
-    public void testRealizarRetirada() {
-        System.out.println("realizarRetirada");
-        double cantidad = 0.0;
-        PasarelaPago p = null;
-        CajeroAutomatico instance = null;
-        boolean expResult = false;
-        boolean result = instance.realizarRetirada(cantidad, p);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        /* (1) Se crea el objeto de la clase a probar y un mock para simular la
+ clase PasarelaPago
+         */
+        CajeroBajoPrueba = new CajeroAutomatico("1111111111");
+        CajeroBajoPrueba.iniciarSesion("1234");
+        mock = createMock(PasarelaPago.class);
+        /* (2) En estado de "grabación", se le dice al objeto Simulado las
+ llamadas que debe esperar y cómo responder a ellas.
+         */
+        expect(mock.estaBloqueada()).andReturn(false);
+        expect(mock.tieneSaldo(500)).andReturn(true);
+        mock.retirar(500);
+        /* (3) Ahora, el objeto simulado comienza a esperar las llamadas
+         */
+        replay(mock);
+        /* (4) Se programa la prueba del objeto de la clase a probar
+         */
+        boolean result = CajeroBajoPrueba.realizarRetirada(500, mock);
+        assertTrue(result);
+        /* (5) Forzamos a que la ausencia de todas las llamadas previstas sea
+ también un error
+         */
+        verify(mock);
+        /* (6) Se ejecutan instrucciones necesarias de finalización de la prueba
+ y se resetea el mock
+         */
+        CajeroBajoPrueba.cerrarSesion();
+        reset(mock);
     }
-    
 }
