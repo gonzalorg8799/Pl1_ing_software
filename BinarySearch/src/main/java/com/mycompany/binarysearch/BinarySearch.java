f498c9296083bcee1a44d92e2f7682119be8476a
David Salgado Soltero <david.salgados@edu.uah.es>
2026-04-28 12:04:20 +0200

    codigoBS

--- BinarySearch/src/main/java/com/mycompany/binarysearch/BinarySearch.java
+++ BinarySearch/src/main/java/com/mycompany/binarysearch/BinarySearch.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.binarysearch;
 
 /**
@@ -10,7 +9,28 @@ package com.mycompany.binarysearch;
  */
 public class BinarySearch {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static int binarySearch(int[] A, int x) {
+// search space is `A[left…right]`
+        int left = 0, right = A.length - 1;
+// loop till the search space is exhausted
+        while (left <= right) {
+// find the mid-value in the search space and
+// compares it with the target
+            int mid = (left + right) / 2;
+// key is found
+            if (x == A[mid]) {
+                return mid;
+            } // discard all elements in the right search space,
+            // including the middle element
+            else if (x < A[mid]) {
+                right = mid - 1;
+            } // discard all elements in the left search space,
+            // including the middle element
+            else {
+                left = mid + 1;
+            }
+        }
+// `x` doesn't exist in the array
+        return -1;
     }
 }
